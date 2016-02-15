1725769a56208f8f09d05e5b12b89347742bb9d2
Deryugin <deryugin.denis@gmail.com>
2016-02-15 18:55:22 +0300

    (input_generator) Implement simple local-vote algo
    Use 4 neighbours. Outside of the field there are just zeroes

--- input_gen.sh
+++ input_gen.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 
 INPUT_FILE=input
-echo "" > $INPUT_FILE
+DEV_COUNT=100
+echo "" > $INPUT_FILE # Clear file
+value=
 
+# Initial values init
+for ((i = 0; i < 100; i++))
+do
+	value[$i]=$((RANDOM%256))
+done
+# Generate input
 for i in {1..1000000}
 do
 	n=$((RANDOM%100))
-	val=$((RANDOM % 100))
-	echo $n $val >> $INPUT_FILE
+	o=${value[$n]}
+	neigh_sum=$((${value[$n-1]} + ${value[$n+1]} + ${value[$n+10]} + ${value[$n-10]}))
+	value[$n]=$(($o + 3 * ($neigh_sum - 4 * $o) / 20))
+	echo $n ${value[$n]} >> $INPUT_FILE
 done
