c139f61b1781bb36dedb227a89705dc431dca76e
Deryugin <deryugin.denis@gmail.com>
2017-02-20 18:47:59 +0300

    (backend) Minor refactor

--- backend/server.py
+++ backend/server.py
@@ -17,11 +17,28 @@ angle = []
 cols = 15
 rows = 4
 
+def limit_val(val, inf, sup):
+    if (val > sup):
+        val = sup
+    if (val < inf):
+        val = inf
+    return val
+
 class Wave:
-    count = 3
+    count     = 3
+    walk_rate = 0.01
     def __init__(self):
         self.mean = [randint(0, rows), randint(0, cols)]
         self.strength = 1.
+    def walk(self):
+        self.mean[0] += self.walk_rate * (randint(0, 10) - 5)
+        self.mean[0] = limit_val(self.mean[0], -1, rows + 1)
+
+        self.mean[1] += self.walk_rate * (randint(0, 10) - 5)
+        self.mean[1] = limit_val(self.mean[1], -1, cols + 1)
+
+        self.strength += self.walk_rate * (randint(0, 10) - 4)
+        self.strength = limit_val(self.strength, 0., 1.)
 
 waves = []
 
@@ -45,13 +62,6 @@ for i in range(0, rows):
         p[i].append(0.0)
         angle[i].append(0.)
 
-def limit_val(val, sup, inf):
-    if (val < sup):
-        return sup;
-    if (val > inf):
-        return inf;
-    return val;
-
 r = 0
 
 while 1:
@@ -65,20 +75,14 @@ while 1:
     sleep(0.01)
 
     for w in waves:
-        w.mean[0] += 0.01 * (randint(0, 10) - 5)
-        w.mean[1] += 0.01 * (randint(0, 10) - 5)
-        w.mean[0] = limit_val(w.mean[0], -1, rows + 1);
-        w.mean[1] = limit_val(w.mean[1], -1, cols + 1);
-
-        w.strength += 0.01 * (randint(0, 10) - 4)
-        w.strength = limit_val(w.strength, 0, 1.)
+        w.walk()
 
     for j in range(0, cols):
         for i in range(0, rows):
             p[i][j] = 0.
             for w in waves:
                 p[i][j] += wave_pressure((i, j), w)
-                p[i][j] = limit_val(p[i][j], 0, 255)
+                limit_val(p[i][j], 0, 255)
 
             p[i][j] *= math.cos(angle[i][j])
 '''
